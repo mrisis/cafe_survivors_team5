a5f42b6c5e0784e49ef505465e58a7c6bceeb164
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-09-19 23:14:25 +0430

    Added SignupForm class with WTForms

---
+++ back_cafe/MG/cafe/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import Email, DataRequired, Length, EqualTo, ValidationError
+from cafe.models import Users
+
+
+class SignupForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=3, max=25,
+                                                                              message='Firstname must be in range 3, 25')])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=3, max=25,
+                                                                            message='Lastname must be in range 3, 25')])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    phone = StringField('Phone', validators=[DataRequired(), Length(min=11, max=11, message='Wrong Phone number')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password',
+                                 validators=[DataRequired(),
+                                             EqualTo('password', message='Confirm password must be equal to Password')])
+
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("This Email is already exist.")
+
+    def validate_phone(self, phone):
+        user = Users.query.filter_by(phone=phone.data).first()
+        if user:
+            raise ValidationError("This Phone number is already exist.")
+
