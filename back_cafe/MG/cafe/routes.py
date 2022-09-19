c0bc5638a8bb299aa0876e6631281f47d7e9e75a
mohamadmahdifarokhi <mohamadmahdifarokhi@gmail.com>
2022-09-19 20:52:38 +0330

    Add order template

--- back_cafe/MG/cafe/routes.py
+++ back_cafe/MG/cafe/routes.py
@@ -2,10 +2,10 @@ from cafe import app
 from flask import render_template
 
 
-
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/signup')
 def signup():
@@ -15,3 +15,8 @@ def signup():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/order')
+def order():
+    return render_template('order.html')
