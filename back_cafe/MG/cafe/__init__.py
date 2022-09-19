1ba9890d3c0671f59c6b7a80049756c6690a55ec
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-09-19 23:13:36 +0430

    Modified signup route and configured secret key, used bcrypt for hashing password

--- back_cafe/MG/cafe/__init__.py
+++ back_cafe/MG/cafe/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "49f4d03ee119ec59346acb4c953a4220"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../cafe.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from cafe import routes
