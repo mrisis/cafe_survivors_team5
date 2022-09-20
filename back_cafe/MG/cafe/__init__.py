56fadb47355a489b705d437af08e84e7ea2e37ba
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-09-21 00:20:43 +0430

    __init__.py | imported loginmanager and configured login_manager | Mehdi Mirzaie

--- back_cafe/MG/cafe/__init__.py
+++ back_cafe/MG/cafe/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "49f4d03ee119ec59346acb4c953a4220"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../cafe.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+login_manager.login_message = 'Please login'
 
 from cafe import routes
