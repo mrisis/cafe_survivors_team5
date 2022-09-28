29604b8563463c5dd14d7bb3c0ffeb654d617fc1
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-09-28 15:28:39 +0330

    forms.py | modify custom validation for phone number | Parsa AhmariPour

--- back_cafe/MG/cafe/forms.py
+++ back_cafe/MG/cafe/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import Email, DataRequired, Length, EqualTo, ValidationError
 from cafe.models import Users
+import re
 
 
 class SignupForm(FlaskForm):
@@ -11,7 +12,7 @@ class SignupForm(FlaskForm):
                                                                             message='Lastname must be in range 3, 25')])
     email = StringField('Email', validators=[DataRequired(), Email()])
     phone_number = StringField('Phone',
-                               validators=[DataRequired(), Length(min=11, max=11, message='Wrong Phone number')])
+                               validators=[DataRequired(), Length(min=11, max=13, message='Wrong Phone number')])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_pass = PasswordField('Confirm Password',
                                  validators=[DataRequired(),
@@ -23,8 +24,9 @@ class SignupForm(FlaskForm):
             raise ValidationError("This Email is already exist.")
 
     def validate_phone_number(self, phone_number):
-        user = Users.query.filter_by(phone_number=phone_number.data).first()
-        if user:
+        if not re.match(r'^(\+989|09)+\d{9}$', phone_number.data):
+            raise ValidationError("Invalid Phone number.")
+        elif Users.query.filter_by(phone_number=phone_number.data).first():
             raise ValidationError("This Phone number is already exist.")
 
 
