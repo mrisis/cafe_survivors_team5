0f77673bf224ccbd7ff25b45892573612abf20d8
khodetitus <masoudpro2@gmail.com>
2022-10-12 00:56:47 +0330

    routes.py | modify file | khodetitus

--- back_cafe/MG/cafe/routes.py
+++ back_cafe/MG/cafe/routes.py
@@ -4,6 +4,7 @@ from flask_login import login_user, current_user, login_required, logout_user
 from cafe import app, bcrypt, db
 from cafe.forms import SignupForm, LoginForm, UpdateProfileForm
 from cafe.models import Users, Menuitems, Orders, Tables, Receipts
+from suds.client import Client
 
 
 @app.route('/')
@@ -75,6 +76,7 @@ def order():
                 orders.append(
                     Orders(table=table, number=int(v), status=False, user=current_user, menuitem=item, receipts=1))
         final_discount = total_price_without_discount - total_price
+        session['total_price'] = total_price
         return render_template('order.html', orders=orders,
                                total_price=total_price,
                                counter=counter, table=table, final_discount=final_discount)
@@ -181,3 +183,53 @@ def up_session():
     print(response)
     session[response.replace('_', ' ')] = str(int(session[response.replace('_', ' ')]) + 1)
     return response
+
+
+# -*- coding: utf-8 -*-
+
+# Sample Flask ZarinPal WebGate with SOAP
+
+__author__ = 'Mohammad Reza Kamalifard, Hamid Feizabadi'
+__url__ = 'reyhoonsoft.ir , rsdn.ir'
+__license__ = "GPL 2.0 http://www.gnu.org/licenses/gpl-2.0.html"
+
+MMERCHANT_ID = '41b3a452-5a8c-4f34-86d8-84ba6e87413d'  # Required
+ZARINPAL_WEBSERVICE = 'https://www.zarinpal.com/pg/services/WebGate/wsdl'  # Required  # Amount will be based on Toman  Required
+description = u'توضیحات تراکنش تستی'  # Required
+email = 'masoudpro2@gmail.com'  # Optional
+mobile = '09120572655'  # Optional
+
+
+@app.route('/request/')
+def send_request():
+    amount = 2000  # Toman / Required
+    client = Client(ZARINPAL_WEBSERVICE)
+    result = client.service.PaymentRequest(MMERCHANT_ID,
+                                           amount,
+                                           description,
+                                           email,
+                                           mobile,
+                                           str(url_for('verify', _external=True)))
+    if result.Status == 100:
+        return redirect('https://www.zarinpal.com/pg/StartPay/' + result.Authority)
+    else:
+        return 'Error'
+
+
+@app.route('/verify/', methods=['GET', 'POST'])
+def verify():
+    amount = 2000  # Toman / Required
+    client = Client(ZARINPAL_WEBSERVICE)
+    if request.args.get('Status') == 'OK':
+        result = client.service.PaymentVerification(MMERCHANT_ID,
+                                                    request.args['Authority'],
+                                                    amount)
+        if result.Status == 100:
+            flash('Transaction success. RefID: ' + str(result.RefID), 'success')
+            return redirect(url_for('profile'))
+        elif result.Status == 101:
+            return 'Transaction submitted : ' + str(result.Status)
+        else:
+            return 'Transaction failed. Status: ' + str(result.Status)
+    else:
+        return 'Transaction failed or canceled by user'
