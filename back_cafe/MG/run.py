ae5146669df469c25007388a9fb811b4c4040969
khodetitus <masoudpro2@gmail.com>
2022-10-12 00:57:31 +0330

    run.py | modify file | khodetitus

--- back_cafe/MG/run.py
+++ back_cafe/MG/run.py
@@ -1,4 +1,16 @@
-from cafe import app
+from cafe import app, db
+
+# from cafe.models import Tables
+# import atexit
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    # def exit_handler():
+    #     tables = Tables.query.all()
+    #     for table in tables:
+    #         table.use = False
+    #         db.session.commit()
+    #
+    #
+    # atexit.register(exit_handler)
