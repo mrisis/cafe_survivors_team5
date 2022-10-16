7a5c2f5a7c7e09298e4d8d5726b81d98a43a4183
mohamadmahdifarokhi <108929710+mohamadmahdifarokhi@users.noreply.github.com>
2022-10-16 20:50:03 +0330

    admin.py | Change admin.py | mti

--- back_cafe/MG/admin.py
+++ back_cafe/MG/admin.py
@@ -6,12 +6,10 @@ parser = argparse.ArgumentParser(description='change to admin_user')
 
 parser.add_argument('--phone', type=str, help='user_id')
 
-
-args=parser.parse_args()
+args = parser.parse_args()
 
 if args.phone:
-    user = Users.query.filter_by(phone_number = args.phone).first()
+    user = Users.query.filter_by(phone_number=args.phone).first()
     user.admin = True
     db.session.commit()
     print(f'User {user} is now admin')
-
