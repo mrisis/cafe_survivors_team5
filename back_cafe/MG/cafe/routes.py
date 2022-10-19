1cc5d328d9c6cbcb5a9cb1af3ecaeba66d67cd2a
PArSA AhmariPour <parsa.ahmaripour@gmail.com>
2022-10-20 00:00:08 +0330

    routes.py | deleted some item | Parsa Ahmaripour

--- back_cafe/MG/cafe/routes.py
+++ back_cafe/MG/cafe/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, jsonify, make_response, Response, session
+from flask import render_template, flash, redirect, url_for, request,  session
 from flask_login import login_user, current_user, login_required, logout_user
 from cafe import admin
 from cafe import app, bcrypt, db
